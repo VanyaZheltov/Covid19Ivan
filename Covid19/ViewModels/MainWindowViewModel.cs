68df9da78030ccebcf2fec1ae869fd1a4358773e
алексей <zheltov86@live.ru>
2020-07-21 16:29:11 +0300

    Добавил command

---
+++ Covid19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Covid19.ViewModels.Base;
+
+namespace Covid19.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок Окна
+        private string _Title = "Анализ статистики Covid19";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+
+
+        #region Status : string - DESCRIPTION
+        ///<summary>Статус программы(Перменная)</summary>
+        private string _Status = "Готов!";
+
+        ///<summary>Статус Программы(Свойство)</summary>
+        public string Status
+        {
+          get => _Status;
+          set => Set(ref _Status, value);
+        }
+        #endregion
+
+    }
+}
