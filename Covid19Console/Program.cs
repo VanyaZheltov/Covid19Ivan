e0598f3744d81940f25c94972dd8fe07b367eeea
алексей <zheltov86@live.ru>
2020-07-21 17:52:04 +0300

    Загрузка файла

---
+++ Covid19Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace Covid19Console
+{
+    class Program
+    {
+        const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+        static void Main(string[] args)
+        {
+
+            var client = new HttpClient();
+            var response = client.GetAsync(data_url).Result;
+            var csv_str = response.Content.ReadAsStringAsync().Result;
+            Console.ReadLine();
+           
+           
+        }
+    }
+
+}
