79082698569a2f3e0da569170b5cb5b7423bb82d
алексей <zheltov86@live.ru>
2020-07-21 18:54:19 +0300

    Добавление моделей и значений для графика

---
+++ Covid19/Models/Country.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Covid19.Models
+{
+    internal class PlaceInfo
+    {
+        public string Name { get; set; }
+
+        public Point Location { get; set; }
+
+        public IEnumerable<ConfirmedCount> Count { get; set; }
+    }
+    
+    internal class CountryInfo : PlaceInfo
+    {
+        public IEnumerable<ProvinceInfo> ProvinceCount { get; set; }
+    }
+
+    internal class ProvinceInfo : PlaceInfo { }
+
+    internal struct ConfirmedCount
+    {
+        public DateTime Date { get; set; }
+
+        public int Count { get; set; }
+    }
+
+    internal struct DataPoint
+    {
+        public double XValue { get; set; }
+
+        public double YValue { get; set; }
+    }
+
+
+
+}
--- Covid19/ViewModels/MainWindowViewModel.cs
+++ Covid19/ViewModels/MainWindowViewModel.cs
@@ -6,12 +6,27 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using Covid19.Infrastructure.Commands;
+using Covid19.Models;
 using Covid19.ViewModels.Base;
 
 namespace Covid19.ViewModels
 {
     class MainWindowViewModel : ViewModel
     {
+        #region TestDataPoints : IEnumerable<DataPoint>
+
+        /// <summary>
+        ///  Тестовый набор данных для графиков
+        /// </summary>
+        private IEnumerable<DataPoint> _TestDataPoints;
+
+        /// <summary>
+        ///  Тестовый набор данных для графиков
+        /// </summary>
+        public IEnumerable<DataPoint> TestDataPoint { get => _TestDataPoints; set => Set(ref _TestDataPoints, value); }
+
+        #endregion
+
         #region Заголовок Окна
         private string _Title = "Анализ статистики Covid19";
 
@@ -57,6 +72,17 @@ namespace Covid19.ViewModels
             CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecuted);
 
             #endregion
+
+
+            var data_points = new List<DataPoint>((int)(360/0.1));
+            for(var x = 0d; x <= 360; x+= 0.1)
+            {
+                const double to_rad = Math.PI / 180;
+                var y = Math.Sin(x * to_rad);
+
+                data_points.Add(new DataPoint { XValue = x, YValue = y });
+            }
+            TestDataPoint = data_points;
         }
     }
 }
