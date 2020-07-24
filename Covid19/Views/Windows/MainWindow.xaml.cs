97e9673d2f5664bb244a3519abae3811f6db479e
алексей <zheltov86@live.ru>
2020-07-24 17:41:53 +0300

    Добавление виртуализации

--- Covid19/Views/Windows/MainWindow.xaml.cs
+++ Covid19/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Covid19.Models.Decanat;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,29 @@ namespace Covid19.Views.Windows
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow() => InitializeComponent();      
+        public MainWindow() => InitializeComponent();
+
+        private void GroupsCollection_OnFilter(object sender, FilterEventArgs e)
+        {
+            if (!(e.Item is Group group)) return;
+            if (group.Name is null) return;
+
+
+            var filter_text = GroupNameFilterText.Text;
+            if (filter_text.Length == 0) return;
+
+
+            if (group.Name.ToLower().Contains(filter_text) || group.Name.Contains(filter_text)) return;
+            if (group.Description != null && group.Description.Contains(filter_text)) return;
+
+            e.Accepted = false;
+        }
+
+        private void GroupNameFilterTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var text_box = (TextBox) sender;
+            var collection = (CollectionViewSource)text_box.FindResource("GroupsCollection");
+            collection.View.Refresh();
+        }
     }
 }
